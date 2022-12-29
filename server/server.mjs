f1647c6a18117dbcb440ef9ca84bf515704d8352
ZhykharievAnatolii <nekijt87@gmail.com>
2022-12-29 21:31:17 +0200

    upd

--- server/server.mjs
+++ server/server.mjs
@@ -1,12 +1,36 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+import pg from "pg";
 
-
+const {Client}=pg;
 const server=fastify({
     logger:true,
 
 });
+const client=new Client({
+    user:"myuser",
+    password:"mypass",
+    database:"sales",
+    port:5432,
+    host:"localhost",
+});
+server.register(fastifyCors);
+
+server.post('/good', {
+    schema:{
+        body:{
+            type:'object',
+
+        }
+    }
+} , async (request,reply)=>{
+    const {name, type, size, price}=request.body
+})
+
+
 server.listen({
     port:3000,
     host:'0.0.0.0'
-})
+}).then(()=>{
+    return client.connect();
+}).catch(err=>console.log(err))
